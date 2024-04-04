27340021df372d0bbdd274c6288e4da5809841d8
Alireza Poodineh <pa789892@outlook.com>
2024-04-04 22:08:21 +0330

    Initially add kryptor cli

---
+++ src/Kryptor.Tool/Arguments.cs
@@ -0,0 +1,33 @@
+﻿using CommandLine.Text;
+
+namespace SAPTeam.Kryptor.Tool
+{
+    internal class Arguments
+    {
+        [Usage(ApplicationAlias = "Kryptor")]
+        public static IEnumerable<Example> Examples => new List<Example>()
+        {
+            new("Encrypt a file", new Arguments() { Encrypt = true, KeyStore = "keystore.kks", File = new string[] { "file", "file2", "..." } }),
+            new("Decrypt a file", new Arguments() { Decrypt = true, KeyStore = "keystore.kks", File = new string[] { "file", "file2", "..." } }),
+            new("Generate new keystore", new Arguments() { Generate = true, KeyStoreSize = 256, KeyStore = "keystore.kks" })
+        };
+
+        [Option('e', "encrypt", SetName = "encrypt", Required = true, HelpText = "Encrypts the given file with given keystore.")]
+        public bool Encrypt { get; set; }
+
+        [Option('d', "decrypt", SetName = "decrypt", Required = true, HelpText = "Decrypts the given file with given keystore.")]
+        public bool Decrypt { get; set; }
+
+        [Option('g', "generate", SetName = "generate", Required = true, HelpText = "Generates a new keystore file.")]
+        public bool Generate { get; set; }
+
+        [Option('k', "keystore-size", SetName = "generate", HelpText = "Specifies the number of keys to be generated for new keystore (Usable with -g). Default value is 256.")]
+        public int KeyStoreSize { get; set; }
+
+        [Value(0, Hidden = true)]
+        public string KeyStore { get; set; }
+
+        [Value(1, Hidden = true)]
+        public IEnumerable<string> File { get; set; }
+    }
+}
---
+++ src/Kryptor.Tool/Program.cs
@@ -0,0 +1,35 @@
+﻿using SAPTeam.Kryptor;
+using SAPTeam.Kryptor.Tool;
+
+Console.WriteLine("Kryptor Command-Line Interface");
+
+Parser.Default.ParseArguments<Arguments>(args).WithParsed(async opt =>
+{
+    if (opt.Encrypt)
+    {
+        Console.WriteLine($"Reading keystore: {Path.GetFileName(opt.KeyStore)}");
+        KESKeyStore ks = KESKeyStore.FromString(File.ReadAllText(opt.KeyStore));
+        KESProvider kp = new(ks);
+        foreach (var file in opt.File)
+        {
+            Console.WriteLine($"Encrypting {Path.GetFileName(file)}");
+            await kp.EncryptFileAsync(file, file + ".kef");
+        }
+    } else if (opt.Decrypt)
+    {
+        Console.WriteLine($"Reading keystore: {Path.GetFileName(opt.KeyStore)}");
+        KESKeyStore ks = KESKeyStore.FromString(File.ReadAllText(opt.KeyStore));
+        KESProvider kp = new(ks);
+        foreach (var file in opt.File)
+        {
+            Console.WriteLine($"Decrypting {Path.GetFileName(file)}");
+            await kp.DecryptFileAsync(file, file + ".decrypted");
+        }
+    } else if (opt.Generate)
+    {
+        Console.WriteLine($"Generating keystore with {opt.KeyStoreSize} keys");
+        KESKeyStore ks = KESKeyStore.Generate(opt.KeyStoreSize);
+        File.WriteAllText(opt.KeyStore, ks.ToString());
+        Console.WriteLine($"Keystore is saved to {opt.KeyStore}");
+    }
+});
