91fc2731b7ed9c83814e9f04fd874347026c0783
Alireza Poodineh <pa789892@outlook.com>
2024-04-10 21:04:32 +0330

    Fix kryptor cli crashes

--- src/Kryptor.Tool/Program.cs
+++ src/Kryptor.Tool/Program.cs
@@ -1,35 +1,43 @@
 ﻿using SAPTeam.Kryptor;
 using SAPTeam.Kryptor.Tool;
 
-Console.WriteLine("Kryptor Command-Line Interface");
-
-Parser.Default.ParseArguments<Arguments>(args).WithParsed(async opt =>
+var result = Parser.Default.ParseArguments<Arguments>(args);
+if (result == null || result.Value == null)
 {
-    if (opt.Encrypt)
+    return 0x1;
+}
+
+Console.WriteLine("Kryptor Command-Line Interface");
+Arguments opt = result.Value;
+
+if (opt.Encrypt)
+{
+    Console.WriteLine($"Reading keystore: {Path.GetFileName(opt.KeyStore)}");
+    KESKeyStore ks = KESKeyStore.FromString(File.ReadAllText(opt.KeyStore));
+    KESProvider kp = new(ks);
+    foreach (var file in opt.File)
     {
-        Console.WriteLine($"Reading keystore: {Path.GetFileName(opt.KeyStore)}");
-        KESKeyStore ks = KESKeyStore.FromString(File.ReadAllText(opt.KeyStore));
-        KESProvider kp = new(ks);
-        foreach (var file in opt.File)
-        {
-            Console.WriteLine($"Encrypting {Path.GetFileName(file)}");
-            await kp.EncryptFileAsync(file, file + ".kef");
-        }
-    } else if (opt.Decrypt)
-    {
-        Console.WriteLine($"Reading keystore: {Path.GetFileName(opt.KeyStore)}");
-        KESKeyStore ks = KESKeyStore.FromString(File.ReadAllText(opt.KeyStore));
-        KESProvider kp = new(ks);
-        foreach (var file in opt.File)
-        {
-            Console.WriteLine($"Decrypting {Path.GetFileName(file)}");
-            await kp.DecryptFileAsync(file, file + ".decrypted");
-        }
-    } else if (opt.Generate)
-    {
-        Console.WriteLine($"Generating keystore with {opt.KeyStoreSize} keys");
-        KESKeyStore ks = KESKeyStore.Generate(opt.KeyStoreSize);
-        File.WriteAllText(opt.KeyStore, ks.ToString());
-        Console.WriteLine($"Keystore is saved to {opt.KeyStore}");
+        Console.WriteLine($"Encrypting {Path.GetFileName(file)}");
+        await kp.EncryptFileAsync(file, file + ".kef");
     }
-});
+}
+else if (opt.Decrypt)
+{
+    Console.WriteLine($"Reading keystore: {Path.GetFileName(opt.KeyStore)}");
+    KESKeyStore ks = KESKeyStore.FromString(File.ReadAllText(opt.KeyStore));
+    KESProvider kp = new(ks);
+    foreach (var file in opt.File)
+    {
+        Console.WriteLine($"Decrypting {Path.GetFileName(file)}");
+        await kp.DecryptFileAsync(file, file + ".decrypted");
+    }
+}
+else if (opt.Generate)
+{
+    Console.WriteLine($"Generating keystore with {opt.KeyStoreSize} keys");
+    KESKeyStore ks = KESKeyStore.Generate(opt.KeyStoreSize);
+    File.WriteAllText(opt.KeyStore, ks.ToString());
+    Console.WriteLine($"Keystore is saved to {opt.KeyStore}");
+}
+
+return 0x0;
