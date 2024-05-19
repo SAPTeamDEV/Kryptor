fccf17d7d660da62ce710c698d91a00bd89182b2
Alireza Poodineh <pa789892@outlook.com>
2024-05-19 17:46:29 +0330

    Update InteractiveTests

--- test/Kryptor.InteractiveTests/Program.cs
+++ test/Kryptor.InteractiveTests/Program.cs
@@ -9,70 +9,78 @@ Checker.CheckSize(args);
 return;
 */
 
+string text;
+string input = null;
+int mult = 0;
+
 while (true)
 {
-    if (!File.Exists(Path.Join(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "testkey")))
+    if (!File.Exists("test.kks"))
     {
-        File.WriteAllBytes(Path.Join(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "testkey"), KeyStore.Generate().Raw);
+        File.WriteAllBytes("test.kks", KeyStore.Generate().Raw);
     }
 
-    Kes kp = new(new StandaloneKeyCryptoProvider(new KeyStore(File.ReadAllBytes(Path.Join(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "testkey")))));
+    Kes kp = new(new StandaloneKeyCryptoProvider(new KeyStore(File.ReadAllBytes("test.kks"))));
 
     bool printlog = args.Length < 1;
+    
+    Console.Write("Enter text");
 
-    if (args.Length == 1)
+    if (!string.IsNullOrEmpty(input))
     {
-        /*
-        if (args[0].EndsWith(".enc"))
+        Console.Write($" ({input})");
+    }
+
+    Console.Write(": ");
+
+    string? tIn = Console.ReadLine();
+    
+    if (!string.IsNullOrEmpty(tIn))
+    {
+        input = tIn;
+    }
+
+    Console.Write("Enter multiplier");
+
+    if (mult > 0)
+    {
+        Console.Write($" ({mult})");
+    }
+
+    Console.Write(": ");
+
+    try
+    {
+        string? tMul = Console.ReadLine();
+
+        if (!string.IsNullOrEmpty(tMul))
         {
-            kp.DecryptFile(args[0], args[0] + ".orig");
+            mult = int.Parse(tMul);
         }
-        else
-        {
-            kp.EncryptFile(args[0], args[0] + ".enc");
-        }
-        */
-        Console.WriteLine("success");
-        Console.ReadKey();
-        return 0;
+    }
+    catch (Exception)
+    {
+        mult = 1;
+    }
+
+    text = string.Concat(input.Repeat(mult));
+
+    var enc = kp.EncryptBlockAsync(Encoding.UTF8.GetBytes(text)).Result;
+    Console.WriteLine($"Text Size: {text.Length}, Cipher Length: {enc.Length}");
+    if (printlog)
+    {
+        Console.WriteLine(Convert.ToBase64String(enc));
+        Console.WriteLine();
+    }
+
+    var dec = Encoding.UTF8.GetString(kp.DecryptBlockAsync(enc).Result);
+    if (printlog)
+    {
+        Console.WriteLine(dec);
     }
     else
     {
-        string text;
-        Console.Write("Enter text: ");
-        var input = Console.ReadLine();
-
-        Console.Write("Enter multiplier: ");
-        int mult;
-
-        try
-        {
-            mult = int.Parse(Console.ReadLine());
-        }
-        catch (Exception)
-        {
-            mult = 1;
-        }
-
-        text = string.Concat(input.Repeat(mult));
-
-        var enc = kp.EncryptBlockAsync(Encoding.UTF8.GetBytes(text)).Result;
-        Console.WriteLine($"Text Size: {text.Length}, Cipher Length: {enc.Length}");
-        if (printlog)
-        {
-            Console.WriteLine(Convert.ToBase64String(enc));
-            Console.WriteLine();
-        }
-
-        var dec = Encoding.UTF8.GetString(kp.DecryptBlockAsync(enc).Result);
-        if (printlog)
-        {
-            Console.WriteLine(dec);
-        }
-        else
-        {
-            Console.WriteLine("Success");
-        }
+        Console.WriteLine("Success");
     }
 
     Console.WriteLine();
