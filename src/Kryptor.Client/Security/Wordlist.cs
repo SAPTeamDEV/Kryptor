16b62372b7fde281c864e0f33fd31eb6f07e2dc7
Alireza Poodineh <itsaeliux@gmail.com>
2024-07-23 18:27:25 +0330

    Add AppContextBase

--- src/Kryptor.Client/Security/Wordlist.cs
+++ src/Kryptor.Client/Security/Wordlist.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Text;
 
-namespace SAPTeam.Kryptor.Security
+namespace SAPTeam.Kryptor.Client.Security
 {
     /// <summary>
     /// Provides a performance-friendly wordlist checker.
