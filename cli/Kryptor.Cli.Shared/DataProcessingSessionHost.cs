4ca39621ed6b83d557e57681b8e1fc9956ee8419
Alireza Poodineh <itsaeliux@gmail.com>
2024-07-28 00:04:15 +0330

    Optimize keystore loading

--- cli/Kryptor.Cli.Shared/DataProcessingSessionHost.cs
+++ cli/Kryptor.Cli.Shared/DataProcessingSessionHost.cs
@@ -8,8 +8,12 @@ namespace SAPTeam.Kryptor.Cli
     public class DataProcessingSessionHost : CliSessionHost
     {
         public int BlockSize { get; }
+
         public CryptoProviderConfiguration Configuration { get; }
-        public KeyStore KeyStore { get; }
+
+        public KeyStore KeyStore { get; private set; }
+        string ks;
+
         public string[] Files {  get; }
 
         public DataProcessingSessionHost(int blockSize, string provider, bool continuous, bool removeHash, bool dbp, string keyStore, string[] files)
@@ -24,22 +28,31 @@ namespace SAPTeam.Kryptor.Cli
                 DynamicBlockProccessing = dbp,
             };
 
-            if (File.Exists(keyStore))
+            Files = files;
+
+            ks = keyStore;
+        }
+
+        public override void Start()
+        {
+            base.Start();
+
+            Console.WriteLine("Loading keystore");
+
+            if (File.Exists(ks))
             {
-                var data = File.ReadAllBytes(keyStore);
+                var data = File.ReadAllBytes(ks);
                 KeyStore = new KeyStore(data);
             }
-            else if (TransformerToken.IsValid(keyStore))
+            else if (TransformerToken.IsValid(ks))
             {
-                var token = TransformerToken.Parse(keyStore);
+                var token = TransformerToken.Parse(ks);
                 KeyStore = Utilities.GenerateKeyStoreFromToken(token);
             }
             else
             {
-                throw new FileNotFoundException(keyStore);
+                throw new FileNotFoundException(ks);
             }
-
-            Files = files;
         }
     }
 }
