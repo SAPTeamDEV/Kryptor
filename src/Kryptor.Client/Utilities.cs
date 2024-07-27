c617f2ba9d4798206cb01f6777369cb36b11bfcc
Alireza Poodineh <itsaeliux@gmail.com>
2024-07-27 17:03:02 +0330

    Add new utilities to Client

--- src/Kryptor.Client/Utilities.cs
+++ src/Kryptor.Client/Utilities.cs
@@ -11,10 +11,30 @@ namespace SAPTeam.Kryptor.Client
     /// </summary>
     public static class Utilities
     {
+        /// <summary>
+        /// Gets the short string representation of the given version.
+        /// </summary>
+        /// <param name="verStr">Version string to process.</param>
+        /// <returns></returns>
         public static string GetShortVersionString(string verStr)
         {
             var ver = new Version(verStr);
             return string.Join(".", ver.Major, ver.Minor, ver.Build);
+        }
+
+        /// <summary>
+        /// Generates keystore based on given token.
+        /// </summary>
+        /// <param name="token">
+        /// Source token that has the required data to generate keystore.
+        /// </param>
+        /// <returns></returns>
+        public static KeyStore GenerateKeyStoreFromToken(TransformerToken token)
+        {
+            ITranformer tranformer = Transformers.GetTranformer(token);
+            byte[] buffer = new byte[token.KeySize * 32];
+            tranformer.Generate(buffer, token.Rotate);
+            return new KeyStore(buffer);
         }
     }
 }
