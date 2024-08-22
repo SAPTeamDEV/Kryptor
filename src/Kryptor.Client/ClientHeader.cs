6d40c8fe63ff000dea4eaaafb5fdb19da20ce0d3
Alireza Poodineh <itsaeliux@gmail.com>
2024-08-22 13:32:14 +0330

    Move serial generator to ClientHeader

--- src/Kryptor.Client/ClientHeader.cs
+++ src/Kryptor.Client/ClientHeader.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 
+using SAPTeam.Kryptor.Generators;
+
 namespace SAPTeam.Kryptor.Client
 {
     /// <summary>
@@ -7,6 +9,8 @@ namespace SAPTeam.Kryptor.Client
     /// </summary>
     public class ClientHeader : Header
     {
+        private static CryptoRandom crng = new CryptoRandom();
+
         /// <summary>
         /// Gets or sets the name of the encryptor client application.
         /// </summary>
@@ -26,5 +30,19 @@ namespace SAPTeam.Kryptor.Client
         /// Gets or sets the unique identifier of the encrypted file.
         /// </summary>
         public string Serial { get; set; }
+
+        /// <summary>
+        /// Generates a new unique identifier for the header and associates it with the <see cref="Serial"/> property.
+        /// </summary>
+        public void GenerateSerial()
+        {
+            string[] serial = new string[8];
+            for (int i = 0; i < serial.Length; i++)
+            {
+                serial[i] = crng.Next(0x1869F).ToString("D5");
+            }
+
+            Serial = string.Join("-", serial);
+        }
     }
 }
